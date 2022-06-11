b0630f8cd116accd309c689583a8cf3309f63900
zikruljustika <zikrul@justika.com>
2022-06-11 17:01:04 +0800

    create cart component
    on this commit we create cart component and add and substract scenario and UI. we also implement some flow for this app that this app is used to simulate online shopping cart

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React, { useState } from 'react';
 import { Pressable, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import * as Progress from "react-native-progress"
+import * as Progress from "react-native-progress";
 import { useEffect } from 'react/cjs/react.development';
-
+import Cart from './src/component/Cart'
 
 const App = () => {
   const [progress, setProgress] = useState(0);
-  const [intervalId, setIntervalId] = useState(999)
+  const [intervalId, setIntervalId] = useState();  
+  const [totalItems, setTotalItems] = useState(0)
+
+  const [loadingText, setLoadingText] = useState("Loading, Please Wait...")
 
   useEffect(() => {
-    console.log("useEffect")
     loadingProgress(0)
   }, [])
 
   const loadingProgress = (currentProgress) => {
+
+    setLoadingText("Loading...")
     let progressTemp = currentProgress
     let interval = setInterval(() => {
       
-      progressTemp += 0.01;
+      progressTemp += 0.02;
       if (progressTemp > 1) {
         console.log(progressTemp)
         progressTemp = 1
@@ -32,6 +36,7 @@ const App = () => {
   }
 
   const onLongPressed = () => {
+    setLoadingText("Paused")
     clearInterval(intervalId)
   }
 
@@ -42,40 +47,93 @@ const App = () => {
   const resetProgressBar = () => {
     clearInterval(intervalId)
     loadingProgress(0)
+    setTotalItems(0)
+  }
+
+  const addItems = () => {
+    setTotalItems(totalItems+1)
+  }
+
+  const decreaseItems = () => {
+    if (totalItems > 0) setTotalItems(totalItems-1)
   }
 
   return (
-    
-      <Pressable 
-        style={styles.container}
-        onPressIn={onLongPressed}
-        onPressOut={onPressOut}
-        >
-        <View style={styles.logBox}>
-          <Progress.Bar color={"white"}progress={progress} width={300}></Progress.Bar>
-        </View>
+    <View style={styles.wrapper}>
 
-        <TouchableOpacity style={styles.button} activeOpacity={0.8} onPress={resetProgressBar}>
-          <Text style={{color:"white", fontSize:16}}>Restart</Text>
-        </TouchableOpacity>
+      {progress<1 ? (<Pressable 
+          style={[styles.container, {flex:1}]}
+          onPressIn={onLongPressed}
+          onPressOut={onPressOut}
+          >
+          <View style={styles.logBox}>
+            <Text style={{marginBottom:16, fontSize:32, color: "black"}}>{loadingText}</Text>
 
+            <Progress.Bar color={"blue"} progress={progress} width={300} height={10}></Progress.Bar>
 
-      </Pressable>
+            <Text style={{marginTop:32}}> Press and hold to pause loading, release to resume</Text>
+          </View>
+        </Pressable>) : (
+        <>
+          <Cart totalItems = {totalItems}/>
+
+          <View 
+            style={[styles.container, {flexDirection:'row'}]}>
+            <TouchableOpacity disabled={totalItems <= 0}  underlayColor='#fff' style={
+              totalItems > 0 ?
+              [styles.button, styles.bgColorRed] : [styles.button, styles.bgColorGrey]
+              } activeOpacity={0.8} onPress={decreaseItems}>
+                <Text style={{color:"white", fontSize: 32}}>-</Text>
+            </TouchableOpacity>
+            
+            <TouchableOpacity style={[styles.button, styles.bgColorGreen]} activeOpacity={0.8} onPress={addItems}>
+                <Text style={{color:"white", fontSize: 32}}>+</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.container}>
+            <TouchableOpacity style={styles.restartButton} activeOpacity={0.8} onPress={resetProgressBar}>
+              <Text style={{color:"white", fontSize:16}}>Restart</Text>
+            </TouchableOpacity>
+          </View>  
+        </>
+      )}
+      
+    </View>
     
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
     flex: 1,
-    alignItems:"center",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
   },
-  button: {
+  container: {
+    alignItems:"center",
+    justifyContent: 'space-evenly',
+  },
+  restartButton: {
     marginTop:24,
     backgroundColor: "green",
     padding: 8,
     borderRadius:8,
+  },
+  button: {
+    width:70,
+    height:50,
+    borderRadius:50,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  bgColorRed: {
+    backgroundColor: "red"
+  },
+  bgColorGrey: {
+    backgroundColor: "grey"
+  },
+  bgColorGreen: {
+    backgroundColor: "green"
   },
   text: {
     fontSize: 16
@@ -90,8 +148,7 @@ const styles = StyleSheet.create({
     margin: 10,
     borderWidth: StyleSheet.hairlineWidth,
     borderColor: '#f0f0f0',
-    backgroundColor: 'red',
-   alignItems:"center"
+    alignItems:"center"
   }
 });
 
--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,7 +1,47 @@
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StyleSheet
+} from "react-native";
+import React, { useState } from 'react';
 
+import Icon from "react-native-vector-icons/Ionicons"
 
 export default function Card(props) {
 
+    return (
+        <View style = {styles.container}>
+            <Icon name="ios-cart" size={150}/>
+            { 
+                props.totalItems>0 ? 
+                    (<View style={styles.roundedCartItem}>
+                        <Text style={{color :"white", fontSize:24, fontWeight:"bold"}}>{props.totalItems}</Text>
+                    </View>) : (<></>)
+            }
+        </View>
+    )
+
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems:"center",
+        justifyContent: "center",
+    },
+    roundedCartItem: {
+        position: "absolute",
+        height: 50,
+        justifyContent: "center",
+        width: 50,
+        top:0,
+        left:230,
+        borderRadius:50,
+        backgroundColor: "red",
+        alignItems: "center",
+        color: "white"
+    }
+})
 
 
